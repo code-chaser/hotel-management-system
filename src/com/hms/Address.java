23651bf3a7a7ed8e6cd5854e2521381d0600ee84
Yashvardhan Baid <codechaser2002@gmail.com>
2021-11-09 22:02:50 +0530

    lil progress

---
+++ src/com/hms/Address.java
@@ -0,0 +1,39 @@
+package com.hms;
+
+import java.util.*;
+import java.io.*;
+
+public class Address {
+    private String line1, line2;
+    private String city;
+    private String state;
+    private String pinCode;
+    private String country;
+
+    public Address() {
+        line1 = line2 = city = state = pinCode = country = "";
+    }
+
+    public void takeInput() {
+        System.out.print("\nEnter Address:\n");
+        System.out.print("\nEnter Line 1:\n");
+        System.out.print("\nEnter Line 2:\n");
+        System.out.print("\nEnter Address:\n");
+        System.out.print("\nEnter Address:\n");
+        System.out.print("\nEnter Address:\n");
+        System.out.print("\nEnter Address:\n");
+    }
+
+    public void print() {
+
+    }
+
+    public String addToStr() {
+        String add = "";
+        return add;
+    }
+
+    public void strToAdd(String add) {
+
+    }
+}
