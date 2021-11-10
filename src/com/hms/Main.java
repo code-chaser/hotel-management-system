1c5b0a7da4aceccd1acb9876a186d87789139503
Yashvardhan Baid <codechaser2002@gmail.com>
2021-11-10 21:17:33 +0530

    rename filehandling

--- src/com/hms/Main.java
+++ src/com/hms/Main.java
@@ -1,8 +1,12 @@
 package com.hms;
 
 import java.util.*;
+
+import com.hms.fileHandling.FileHandling;
+
 import java.io.*;
-import com.hms.filehandling.Filehandling;
+
+//create new thread
 
 public class Main {
     public static void main(String[] args) {
